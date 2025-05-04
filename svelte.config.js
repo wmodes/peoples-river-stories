22daefaf21ab281ae420d14aabdb01f3e2c5e6f3
Wes Modes <wmodes@gmail.com>
2025-05-03 19:37:59 -0700

    static>node svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html'
+      out: 'build'
     })
   }
 };
